cc457a0df0e50388f05fdd3f5270506a035ded96
newin <newin@legion.ch>
2016-12-15 22:06:58 +0100

    adding realtime file handler

---
+++ GLnewin/fileWatcher.cpp
@@ -0,0 +1,41 @@
+// only work on linux
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/inotify.h>
+
+#include "fileWatcher.hh"
+
+
+FileWatcher::FileWatcher(const char* f_) : _still(true) {
+    _fd = inotify_init();
+
+    if (_fd < 0 ) {
+	std::cout << "file \"" << f_ << "\" is not valid\n";
+    }
+    _wd = inotify_add_watch(_fd, f_, IN_MODIFY);
+
+    t = std::thread([this]() {
+	    while (_still) {
+		_length = read(_fd, _buffer, 4096);
+		std::cout << "change detected\n";
+	    }
+	    std::cout << "end\n";
+    });
+}
+
+FileWatcher::FileWatcher(std::string& f_)  : FileWatcher(f_.c_str()) {
+}
+
+bool FileWatcher::isModified() {
+    if (_length != 0) {
+	_length = 0;
+	return true;
+    }
+    return false;
+}
+
+FileWatcher::~FileWatcher() {
+    _still = false;
+    inotify_rm_watch(_fd, _wd);
+    close(_fd);
+}
---
+++ GLnewin/fileWatcher.hh
@@ -0,0 +1,21 @@
+#include <thread>
+#include <future>
+#include <iostream>
+
+#define EVENT_BUF_LEN   4096  
+
+class FileWatcher {
+    private:
+	int _length;
+	int _fd;
+	int _wd;
+	bool _still;
+	char _buffer[4096];
+	std::thread t;
+    public:
+	//void(*_callBack)(void);
+	FileWatcher(std::string&);
+	FileWatcher(const char*);
+	bool isModified();
+	~FileWatcher();
+};
