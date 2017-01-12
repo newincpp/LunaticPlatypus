ea8a2c409f122d2d65272cf9b33f8d71783a381a
Sicarde <camille.koziar@gmail.com>
2017-01-13 00:34:39 +0100

    solve alembic pb

--- GLnewin/libs/extractObject.sh
+++ GLnewin/libs/extractObject.sh
@@ -4,8 +4,13 @@ rm -R Objectsoup
 for f in $FILES
 do
      echo "Processing $f"
-     ar -x $f
+     if [ "$f" != "libAlembic.a" ]; then
+	     ar -x $f
+     fi
       # do something on $f
 done
 mkdir -p Objectsoup
+mkdir -p Objectsoup/Alembic
 mv *.o Objectsoup  
+ar -x libAlembic.a
+mv *.o Objectsoup/Alembic
