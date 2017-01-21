574ed82f7f08a9a195590088e03335189a3fc976
_Nyu <newin@protonmail.ch>
2017-01-21 02:55:19 +0100

    adding research for future work

--- GLnewin/Texture.hh
+++ GLnewin/Texture.hh
@@ -2,6 +2,10 @@
 #include "glew.h"
 #include <glm/glm.hpp>
 
+// rewrite this using opengl 4.5 style
+// exemples here: https://bcmpinc.wordpress.com/2015/10/07/copy-a-texture-to-screen/#more-365
+// and here: https://cpp-rendering.io/opengl-azdo-bindless-textures/
+
 class Texture {
     public:
 	GLuint _textureID;
