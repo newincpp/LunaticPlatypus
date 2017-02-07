1dc96c960888b14c8b7d68e5a743618d0729ae05
newin <newin@legion.ch>
2017-02-07 15:35:10 +0100

    making the .a extraction smarter

--- GLnewin/libs/extractObject.sh
+++ GLnewin/libs/extractObject.sh
@@ -1,16 +1,16 @@
 #libAlembic.a  libassimp.a  libGLEW.a  libHalf.a  libhdf5.a  libz.a
 FILES=`ls *.a`
-rm -R Objectsoup
+rm -Rf Objectsoup
+mkdir -p Objectsoup
 for f in $FILES
 do
      echo "Processing $f"
      if [ "$f" != "libAlembic.a" ]; then
-	     ar -x $f
+	 mkdir -p Objectsoup/${f%.*}
+	 cp $f Objectsoup/${f%.*}
+	 cd Objectsoup/${f%.*}
+	 ar -x $f
+	 cd -
      fi
       # do something on $f
 done
-mkdir -p Objectsoup
-mkdir -p Objectsoup/Alembic
-mv *.o Objectsoup  
-ar -x libAlembic.a
-mv *.o Objectsoup/Alembic
